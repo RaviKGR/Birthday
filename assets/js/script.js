b0e250b85f3fb67974562d789aa9993276133440
RaviKGR <ravimuthukgr@gmail.com>
2024-03-16 12:26:05 +0530

    design chnage

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,9 @@
                     '<div class="col-12 col-md-6">' +
                         '<div id="video-background">' +
                             '<video autoplay loop muted>' +
-                                '<source src="/assets/video/production_id_4112431 (1080p).mp4" type="video/mp4">' +    
+                                '<source src="/assets/video/production_id_4112431 (1080p).mp4" type="video/mp4">' + 
+                                '<source src="/assets/video/production_id_4112431 (720p).mp4" type="video/mp4">' +
+                                '<source src="/assets/video/production_id_4112431 (540p).mp4" type="video/mp4">' +   
                                 'Your browser does not support the video tag.' +
                             '</video>' +
                         '</div>' +
@@ -21,8 +23,8 @@
                                 '<h1>“The day is all yours — have fun!”</h1>' +
                                 '<p>"Happy Birthday to the one who brings laughter to every moment, light to every day, and joy to every gathering! May this year be filled with endless adventures, unforgettable memories, and all the happiness your heart can hold. Here\'s to celebrating you today and always, my dear friend!" 🎉🎂🎈</p>' +
                             '</div>' +
-                            '<div class="mt-4 d-flex justify-content-center">'+
-                                '<button id="removeClassButton" class="image-button fw-bolder" id="image_forward" onclick="imageforward()">'+
+                            '<div id="removeClassButton" class="mt-4 d-flex justify-content-center">'+
+                                '<button class="image-button fw-bolder" id="image_forward" onclick="imageforward()">'+
                                     'More'+
                                     '<div class="button__horizontal"></div>'+
                                     '<div class="button__vertical"></div>'+
@@ -39,14 +41,17 @@
     
     function imageforward() {
             console.log(elements);
-            let rovedesplay = document.getElementById("removeClassButton");
-            rovedesplay.classList.add("d-none");
+            let romvedesplay = document.getElementById("removeClassButton");
+            // romvedesplay.classList.add("d-none");
+            
             if (elements.classList.contains("d-none")) {
                 // If the element has class "d-none", remove it
+                romvedesplay.classList.add("d-none");
                 elements.classList.remove("d-none");
                 console.log("Class removed, content visible.");
             } else {
                 // If the element does not have class "d-none", add it
+                romvedesplay.classList.remove("d-none");
                 elements.classList.add("d-none");
                 console.log("Class added, content hidden.");
             };
